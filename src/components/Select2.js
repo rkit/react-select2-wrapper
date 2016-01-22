d4c7b414edb724f84c7e709dc543bcd316a14e99
Linus Nilsson <linus.nilsson@trimma.se>
2016-01-22 08:14:58 +0100

    Fix no-else-after-return

--- src/components/Select2.js
+++ src/components/Select2.js
@@ -60,10 +60,10 @@ export default class Select2 extends Component {
           if (typeof item === 'string' ||
             ((!!item && typeof item === 'object') && Object.prototype.toString.call(item) === '[object String]')) {
             return (<option key={'option-' + k} value={item}>{item}</option>);
-          } else {
-            const { id, text, ...itemParams } = item;
-            return (<option key={'option-' + k} value={id} {...itemParams}>{text}</option>);
           }
+
+          const { id, text, ...itemParams } = item;
+          return (<option key={'option-' + k} value={id} {...itemParams}>{text}</option>);
         })}
       </select>
     );
